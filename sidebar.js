72c6819de645e6292daba502d118d719bee50676
victor-code <vdcloayza@gmail.com>
2023-05-16 22:23:38 -0400

    version 3 beta

--- sidebar.js
+++ sidebar.js
@@ -13,13 +13,13 @@ iconoPaExpandir.addEventListener("click", () => {
       "girar_to_right"
     );
     sidebarContainer.style.gridTemplate =
-      "100px 100px 100px 100px /290px 40px";
+      "100px 100px 100px 100px /50vw 40px";
   } else {
     iconoPaExpandir.classList.remove(
       "girar_to_right"
     );
 
     sidebarContainer.style.gridTemplate =
-      "100px 100px 100px 100px /90px 40px";
+      "100px 100px 100px 100px /12vw 40px";
   }
 });

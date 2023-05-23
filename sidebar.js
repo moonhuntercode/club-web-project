51070507c0bb931ca1bc5cc75ca5018392d1afd8
victor-code <vdcloayza@gmail.com>
2023-05-22 23:59:59 -0400

    beta 2

--- sidebar.js
+++ sidebar.js
@@ -10,18 +10,26 @@ let sidebarContainer = document.querySelector(
   ".sidebarLefContainer"
 );
 window.onload = () => {
-  let elementoObjetivo = document.querySelector(
+  const collection = document.querySelectorAll(
     ".name_of_section"
   );
-  elementoObjetivo.remove();
-  // console.log(elementoObjetivo);
+
+  for (let i = 0; i < collection.length; i++) {
+    collection[i].remove();
+  }
+
+  // elementoObjetivo.classList.add("oculto");
 };
 iconoPaExpandir.addEventListener("click", () => {
   // console.log(iconoPaExpandir.classList);
   if (iconoPaExpandir.classList.length < 1) {
-    iconoPaExpandir.classList.add(
-      "girar_to_right"
-    );
+    setTimeout(() => {
+      createSpan1();
+      createSpan2();
+      createSpan3();
+      createSpan4();
+    }, 500);
+    iconoPaExpandir.classList.add("girar_to_right");
     sidebarContainer.style.gridTemplate =
       "100px 100px 100px 100px /50vw 40px";
 
@@ -29,9 +37,14 @@ iconoPaExpandir.addEventListener("click", () => {
       desaparecerName.innerHTML = NAME_OF_GROUP;
     }, 500);
   } else {
-    iconoPaExpandir.classList.remove(
-      "girar_to_right"
+    const collection = document.querySelectorAll(
+      ".name_of_section"
     );
+
+    for (let i = 0; i < collection.length; i++) {
+      collection[i].remove();
+    }
+    iconoPaExpandir.classList.remove("girar_to_right");
 
     // desaparecerName.style.visibility = "hidden";
     desaparecerName.innerHTML = " ";
@@ -39,3 +52,50 @@ iconoPaExpandir.addEventListener("click", () => {
       "100px 100px 100px 100px /12vw 40px";
   }
 });
+function createSpan1() {
+  const span1 = document.createElement("span");
+  span1.className = "name_of_section";
+  span1.innerHTML = "our groups";
+  let sectionElegida = document.querySelectorAll(
+    ".childrenSidebarLef"
+  )[0];
+  sectionElegida.appendChild(span1);
+  // test of sectiontion
+  // console.log(sectionElegida);
+}
+function createSpan2() {
+  const span = document.createElement("span");
+  span.className = "name_of_section";
+  span.innerHTML = "our restaurants";
+  let sectionElegida = document.querySelectorAll(
+    ".childrenSidebarLef"
+  )[1];
+  sectionElegida.appendChild(span);
+  // test of section
+  // console.log(sectionElegida);
+}
+function createSpan3() {
+  const span = document.createElement("span");
+  span.className = "name_of_section";
+  span.innerHTML = "our favorites";
+  let sectionElegida = document.querySelectorAll(
+    ".childrenSidebarLef"
+  )[2];
+  sectionElegida.appendChild(span);
+  // test of section
+  // console.log(sectionElegida);
+}
+function createSpan4() {
+  const span = document.createElement("span");
+  span.className = "name_of_section";
+  span.innerHTML = "our events";
+  let sectionElegida = document.querySelectorAll(
+    ".childrenSidebarLef"
+  )[3];
+  sectionElegida.appendChild(span);
+  // test of section
+  // console.log(sectionElegida);
+}
+// reemplazando vista2
+const vista2 = document.querySelector("#calendario");
+console.log(vista2);
